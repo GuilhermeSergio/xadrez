fffca66b2784726851116ccc0f039465222ab4f5
GuilhermeSergio <gui.sergiiio@gmail.com>
2022-12-12 20:54:18 -0300

    novo commit

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -36,7 +36,7 @@ public class ChessMatch {
 
 	private void validateSourcePosition(Position position) {
 		if (!board.thereIsAPiece(position)){
-			throw new ChessException("Não existe peca na posicao de origem");
+			throw new ChessException("Nao existe peca na posicao de origem");
 		}
 	}
 	
